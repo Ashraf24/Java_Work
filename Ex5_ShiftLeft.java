956e12bc42f31b3c6c73e7908907c5e0c9100a97
Ashraful Alam <alam24@usa.com>
2020-09-17 20:32:55 +0100

    ch04_loops

---
+++ Ex5_ShiftLeft.java
@@ -0,0 +1,47 @@
+package apollo.exercises.ch04_loops;
+
+public class Ex5_ShiftLeft {
+
+	// This is the main method that is executed as
+	// soon as the program starts.	
+	public static void main(String[] args) {
+	  // Call shiftLeft and print out the result with a few different arrays.
+	
+	
+	// Create a method called shiftLeft
+    // Return an array that is "left shifted" by one -- 
+	// so {6, 2, 5, 3} returns {2, 5, 3, 6}. Y
+	// You may modify and return the given array, or return a new array. 
+	
+	 int a[] = {6, 2, 5, 3};
+	// shiftLeft(a); // => {2, 5, 3, 6}
+	// int b = {1, 2};
+	// shiftLeft(b); // => {2, 1}
+	// int c = {1};
+	// shiftLeft(c); // => {1}
+
+ int n=a.length;
+	
+		
+ int temp=a[0];
+ for(n=0;n<=n-1;n++)
+ {
+   a[n]=a[n+1];
+ }
+ a[n-1]=temp;
+
+	  
+	    	 
+	     }
+	
+	}
+
+
+/*
+ * SAMPLE OUTPUT:
+ *  
+ * {2, 5, 3, 6}
+ * {2, 1}
+ * {1}
+ *  
+ */
